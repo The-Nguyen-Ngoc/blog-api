21dc83c5ca09aa98bd451f2ae80d0ea2e93882da
TheNN <thenguyenngochn@gmail.com>
2022-07-03 18:41:38 +0700

    Initial commit

---
+++ src/test/java/com/thenn/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenn.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
